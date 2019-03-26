61eafa5a3094b771dc00c1730d73c163ab34466d
Unknown <michael90@protonmail.com>
2019-03-26 08:09:11 +0100

    Use correct locale for date lines, proper date format

--- app/src/main/java/ln_zap/zap/historyList/DateLineViewHolder.java
+++ app/src/main/java/ln_zap/zap/historyList/DateLineViewHolder.java
@@ -1,27 +1,28 @@
 package ln_zap.zap.historyList;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+
 import java.util.Date;
-import java.util.Locale;
 
 import androidx.recyclerview.widget.RecyclerView;
 import ln_zap.zap.R;
 
 public class DateLineViewHolder extends RecyclerView.ViewHolder{
-    public TextView mTvDate;
+    private TextView mTvDate;
+    private Context mContext;
     public DateLineViewHolder(View v) {
         super(v);
         mTvDate = v.findViewById(R.id.date);
+        mContext = v.getContext();
     }
 
     public void bindDateItem(DateItem dateItem){
-
-        // ToDo: correct locale!!!!!!!!
-        String dateAsText = new SimpleDateFormat("yyyy-MM-dd", Locale.US)
-                .format(new Date(dateItem.mDate * 1000L));
-        mTvDate.setText(dateAsText);
+        DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, mContext.getResources().getConfiguration().locale);
+        String formattedDate = df.format(new Date(dateItem.mDate * 1000L));
+        mTvDate.setText(formattedDate);
     }
 }
