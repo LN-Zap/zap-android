9ef74b59a134a7bd7a7ca69341b5ca190f1daf39
Unknown <michael90@protonmail.com>
2019-08-28 21:30:01 +0200

    Address feedback

--- app/src/main/java/zapsolutions/zap/util/BlockExplorer.java
+++ app/src/main/java/zapsolutions/zap/util/BlockExplorer.java
@@ -15,21 +15,9 @@ import zapsolutions.zap.interfaces.UserGuardianInterface;
 public class BlockExplorer implements UserGuardianInterface {
 
     private String mUrl;
-    private boolean mSupported;
+    private boolean misNetworkSupported;
     private Context mContext;
 
-
-    private void unsupportedNetwork(String explorer, String network, Context ctx) {
-        new AlertDialog.Builder(ctx)
-                .setTitle(R.string.error_blockExplorer_title)
-                .setMessage(ctx.getResources().getString(R.string.error_blockExplorer_message, explorer, network))
-                .setCancelable(true)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                    }
-                })
-                .show();
-    }
 
     /**
      * Shows transaction details in a browser window using the preferred block explorer.
@@ -40,39 +28,39 @@ public class BlockExplorer implements UserGuardianInterface {
     public void showTransaction(String transactionID, Context ctx) {
         mContext = ctx;
         String explorer = PrefsUtil.getPrefs().getString("blockExplorer", "BlockCypher");
-        boolean mainnet = !Wallet.getInstance().isTestnet();
+        boolean isMainnet = !Wallet.getInstance().isTestnet();
         String networkID = "";
         mUrl = "";
-        mSupported = true;
+        misNetworkSupported = true;
 
         switch (explorer) {
             case "BlockCypher":
-                networkID = mainnet ? "btc" : "btc-testnet";
+                networkID = isMainnet ? "btc" : "btc-testnet";
                 mUrl = "https://live.blockcypher.com/" + networkID + "/tx/" + transactionID;
                 break;
             case "Blockstream":
-                networkID = mainnet ? "" : "testnet/";
+                networkID = isMainnet ? "" : "testnet/";
                 mUrl = "https://blockstream.info/" + networkID + "tx/" + transactionID;
                 break;
             case "Blockstream (v3 Tor)":
-                networkID = mainnet ? "" : "testnet/";
+                networkID = isMainnet ? "" : "testnet/";
                 mUrl = "http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/" + networkID + "tx/" + transactionID;
                 break;
             case "Smartbit":
-                networkID = mainnet ? "www" : "testnet";
+                networkID = isMainnet ? "www" : "testnet";
                 mUrl = "https://" + networkID + ".smartbit.com.au/tx/" + transactionID;
                 break;
             case "Blockchain Reader (Yogh)":
-                mSupported = mainnet;
+                misNetworkSupported = isMainnet;
                 mUrl = "http://srv1.yogh.io/#tx:id:" + transactionID;
                 break;
             case "OXT":
-                mSupported = mainnet;
+                misNetworkSupported = isMainnet;
                 mUrl = "https://oxt.me/transaction/" + transactionID;
                 break;
         }
 
-        if (explorer.equals("Blockstream (v3 Tor)")){
+        if (explorer.equals("Blockstream (v3 Tor)")) {
             startBlockExplorer();
         } else {
             new UserGuardian(mContext, this).privacyBlockExplorer();
@@ -88,39 +76,39 @@ public class BlockExplorer implements UserGuardianInterface {
     public void showAddress(String address, Context ctx) {
         mContext = ctx;
         String explorer = PrefsUtil.getPrefs().getString("blockExplorer", "BlockCypher");
-        boolean mainnet = !Wallet.getInstance().isTestnet();
+        boolean isMainnet = !Wallet.getInstance().isTestnet();
         String networkID = "";
         mUrl = "";
-        mSupported = true;
+        misNetworkSupported = true;
 
         switch (explorer) {
             case "BlockCypher":
-                networkID = mainnet ? "btc" : "btc-testnet";
+                networkID = isMainnet ? "btc" : "btc-testnet";
                 mUrl = "https://live.blockcypher.com/" + networkID + "/address/" + address;
                 break;
             case "Blockstream":
-                networkID = mainnet ? "" : "testnet/";
+                networkID = isMainnet ? "" : "testnet/";
                 mUrl = "https://blockstream.info/" + networkID + "address/" + address;
                 break;
             case "Blockstream (v3 Tor)":
-                networkID = mainnet ? "" : "testnet/";
+                networkID = isMainnet ? "" : "testnet/";
                 mUrl = "http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/" + networkID + "address/" + address;
                 break;
             case "Smartbit":
-                networkID = mainnet ? "www" : "testnet";
+                networkID = isMainnet ? "www" : "testnet";
                 mUrl = "https://" + networkID + ".smartbit.com.au/address/" + address;
                 break;
             case "Blockchain Reader (Yogh)":
-                mSupported = mainnet;
+                misNetworkSupported = isMainnet;
                 mUrl = "http://srv1.yogh.io/#addr:id:" + address;
                 break;
             case "OXT":
-                mSupported = mainnet;
+                misNetworkSupported = isMainnet;
                 mUrl = "https://oxt.me/address/" + address;
                 break;
         }
 
-        if (explorer.equals("Blockstream (v3 Tor)")){
+        if (explorer.equals("Blockstream (v3 Tor)")) {
             startBlockExplorer();
         } else {
             new UserGuardian(mContext, this).privacyBlockExplorer();
@@ -130,19 +118,31 @@ public class BlockExplorer implements UserGuardianInterface {
     @Override
     public void guardianDialogConfirmed(String DialogName) {
         if (DialogName.equals(UserGuardian.BLOCK_EXPLORER)) {
-           startBlockExplorer();
+            startBlockExplorer();
         }
     }
 
-    private void startBlockExplorer(){
-        if (mSupported) {
+    private void unsupportedNetwork(String explorer, String network, Context ctx) {
+        new AlertDialog.Builder(ctx)
+                .setTitle(R.string.error_blockExplorer_title)
+                .setMessage(ctx.getResources().getString(R.string.error_blockExplorer_message, explorer, network))
+                .setCancelable(true)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                })
+                .show();
+    }
+
+    private void startBlockExplorer() {
+        if (misNetworkSupported) {
             // Call the url
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mUrl));
             mContext.startActivity(browserIntent);
         } else {
-            String explorer = PrefsUtil.getPrefs().getString("blockExplorer", "BlockCypher");
-            boolean mainnet = !Wallet.getInstance().isTestnet();
-            unsupportedNetwork(explorer, mainnet ? "mainnet" : "testnet", mContext);
+            String explorer = PrefsUtil.getPrefs().getString("blockExplorer", "Blockstream");
+            boolean isMainnet = !Wallet.getInstance().isTestnet();
+            unsupportedNetwork(explorer, isMainnet ? "mainnet" : "testnet", mContext);
         }
     }
 }
