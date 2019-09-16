2e7a135e65c70e316e87bd8ee974ae5c2cf140bf
R.M <maraph@users.noreply.github.com>
2019-09-16 17:30:37 +0200

    Add NodeParser

---
+++ app/src/main/java/zapsolutions/zap/lightning/LightningParser.java
@@ -0,0 +1,37 @@
+package zapsolutions.zap.lightning;
+
+import androidx.annotation.NonNull;
+
+public class LightningParser {
+
+    private static int NODE_URI_MIN_LENGTH = 66;
+
+    public static LightningNodeUri parseNodeUri(@NonNull String uri) {
+        if (uri.isEmpty() || uri.length() < NODE_URI_MIN_LENGTH) {
+            return null;
+        }
+
+        if (uri.length() == NODE_URI_MIN_LENGTH) {
+            // PubKey only
+            return new LightningNodeUri.Builder().setPubKey(uri).build();
+        }
+
+        if (!uri.contains("@")) {
+            // longer and no @, something is wrong
+            return null;
+        }
+
+        if (uri.indexOf(':') > -1) {
+            // remove port if contained
+            uri = uri.substring(0, uri.indexOf(':'));
+        }
+
+        String[] parts = uri.split("@");
+
+        if (parts.length != 2) {
+            return null;
+        }
+
+        return new LightningNodeUri.Builder().setPubKey(parts[0]).setHost(parts[1]).build();
+    }
+}
