9685673577fd47c7e65b53cc126201e58ebccf19
Michael <michael90@protonmail.com>
2020-08-14 11:29:52 +0200

    Read payment memo without LND

--- app/src/main/java/zapsolutions/zap/transactionHistory/listItems/LnPaymentItem.java
+++ app/src/main/java/zapsolutions/zap/transactionHistory/listItems/LnPaymentItem.java
@@ -3,12 +3,20 @@ package zapsolutions.zap.transactionHistory.listItems;
 import com.github.lightningnetwork.lnd.lnrpc.Payment;
 import com.google.protobuf.ByteString;
 
+import zapsolutions.zap.util.PaymentRequestUtil;
+
 public class LnPaymentItem extends TransactionItem {
     private Payment mPayment;
+    private String mMemo;
 
     public LnPaymentItem(Payment payment) {
         mPayment = payment;
         mCreationDate = payment.getCreationDate();
+
+        if (payment.getPaymentRequest() != null && !payment.getPaymentRequest().isEmpty()) {
+            // This will only be true for payments done with LND 0.7.0-beta and later
+            mMemo = PaymentRequestUtil.getMemo(payment.getPaymentRequest());
+        }
     }
 
     @Override
@@ -23,5 +31,9 @@ public class LnPaymentItem extends TransactionItem {
     @Override
     public ByteString getTransactionByteString() {
         return mPayment.toByteString();
+    }
+
+    public String getMemo() {
+        return mMemo;
     }
 }
--- app/src/main/java/zapsolutions/zap/util/LnurlDecoder.java
+++ app/src/main/java/zapsolutions/zap/util/LnurlDecoder.java
@@ -18,35 +18,13 @@ public class LnurlDecoder {
         String decodedLnurl = null;
         try {
             byte[] decodedBech32 = Bech32.bech32Decode(lnurl, false).second;
+            byte[] regroupedBytes = Bech32.regroupBytes(decodedBech32);
 
-            // Translate the bytes to 5 bit groups, most significant bit first.
-            boolean[] bitArray = new boolean[decodedBech32.length * 5];
-            for (int i = 0; i < decodedBech32.length; i++) {
-                for (int j = 3; j < 8; j++)
-                    bitArray[i * 5 + j - 3] = (decodedBech32[i] & (byte) (128 / Math.pow(2, j))) != 0;
-            }
-
-            // Re-arrange those bits into groups of 8 bits.
-            byte[] regroupedBits = booleanArrayToBytes(bitArray);
-
-            decodedLnurl = new String(regroupedBits);
+            decodedLnurl = new String(regroupedBytes);
         } catch (Exception e) {
             throw new IllegalArgumentException("LNURL decoding failed: " + e.getMessage());
         }
         return decodedLnurl;
-    }
-
-    private static byte[] booleanArrayToBytes(boolean[] input) {
-        byte[] toReturn = new byte[input.length / 8];
-        for (int entry = 0; entry < toReturn.length; entry++) {
-            for (int bit = 0; bit < 8; bit++) {
-                if (input[entry * 8 + bit]) {
-                    toReturn[entry] |= (128 >> bit);
-                }
-            }
-        }
-
-        return toReturn;
     }
 
     public static class NoLnUrlDataException extends Exception {
