649c5d0e6f6d04b7461192b9ddbe6c237595eec6
Michael <michael90@protonmail.com>
2021-02-14 14:26:21 +0100

    Manual setup UI

---
+++ app/src/main/java/zapsolutions/zap/setup/ManualSetup.java
@@ -0,0 +1,16 @@
+package zapsolutions.zap.setup;
+
+import android.os.Bundle;
+
+import zapsolutions.zap.R;
+import zapsolutions.zap.baseClasses.BaseAppCompatActivity;
+
+public class ManualSetup extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_setup);
+
+    }
+}
