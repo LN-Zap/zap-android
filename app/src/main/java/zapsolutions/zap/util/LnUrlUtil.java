6bb02a1d7809c748298d2f4ef4a0af0933aae053
Michael <michael90@protonmail.com>
2021-08-06 11:49:21 +0200

    Replace http client in lnurlUtil

--- app/src/main/java/zapsolutions/zap/util/LnUrlUtil.java
+++ app/src/main/java/zapsolutions/zap/util/LnUrlUtil.java
@@ -4,15 +4,20 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 
-import com.android.volley.Request;
-import com.android.volley.toolbox.StringRequest;
 import com.google.gson.Gson;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Request;
+import okhttp3.Response;
 import zapsolutions.zap.R;
-import zapsolutions.zap.connection.HttpClient;
+import zapsolutions.zap.connection.HttpClientOk;
 import zapsolutions.zap.lnurl.LnUrlResponse;
 import zapsolutions.zap.lnurl.channel.LnUrlChannelResponse;
 import zapsolutions.zap.lnurl.channel.LnUrlHostedChannelResponse;
@@ -55,23 +60,32 @@ public class LnUrlUtil {
                 listener.onError(ctx.getString(R.string.lnurl_unsupported_type), RefConstants.ERROR_DURATION_MEDIUM);
             }
 
-            StringRequest lnurlRequest = new StringRequest(Request.Method.GET, decodedLnUrl,
-                    response -> interpretLnUrlReadResponse(response, listener, ctx),
-                    error -> {
-                        URL url = null;
-                        try {
-                            url = new URL(decodedLnUrl);
-                            String host = url.getHost();
-                            listener.onError(ctx.getString(R.string.lnurl_service_not_responding, host), RefConstants.ERROR_DURATION_SHORT);
-                        } catch (MalformedURLException e) {
-                            String host = ctx.getString(R.string.host);
-                            listener.onError(ctx.getString(R.string.lnurl_service_not_responding, host), RefConstants.ERROR_DURATION_SHORT);
-                            e.printStackTrace();
-                        }
-                    });
-
             ZapLog.v(LOG_TAG, "LNURL: Requesting data...");
-            HttpClient.getInstance().addToRequestQueue(lnurlRequest, "LnUrlWithdrawRequest");
+
+            Request lnurlRequest = new Request.Builder()
+                    .url(decodedLnUrl)
+                    .build();
+
+            HttpClientOk.getInstance().getClient().newCall(lnurlRequest).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    URL url = null;
+                    try {
+                        url = new URL(decodedLnUrl);
+                        String host = url.getHost();
+                        listener.onError(ctx.getString(R.string.lnurl_service_not_responding, host), RefConstants.ERROR_DURATION_SHORT);
+                    } catch (MalformedURLException error) {
+                        String host = ctx.getString(R.string.host);
+                        listener.onError(ctx.getString(R.string.lnurl_service_not_responding, host), RefConstants.ERROR_DURATION_SHORT);
+                        error.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    interpretLnUrlReadResponse(response.body().string(), listener, ctx);
+                }
+            });
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
             ZapLog.e(LOG_TAG, "LNURL is invalid. Decoding failed.");
